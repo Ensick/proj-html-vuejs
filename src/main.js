aa119f771cdb028fa3bb82fe2b5fb6d91cad8041
Ensick <fede.censi96@gmail.com>
2022-12-02 13:11:52 +0100

    Inizio Progetto

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+/*Import icone regular */
+import { faFaceSmile } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret,faFaceSmile)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
